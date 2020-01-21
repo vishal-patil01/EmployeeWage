bcfa6b5d2b6de5759b2be487e7f148c3575c1fe1
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-21 17:07:04 +0530

    ADDED : Calculate Monthly Wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,22 +4,27 @@ echo -e "Welcome To EmployeeWage"
 
 #! Declaring Constants
 readonly RATE_PER_HOUR=20
-readonly EMP_CHECK=$((RANDOM % 3))
 readonly PART_TIME=4
 readonly FULL_TIME=8
 
-#! Calculating Wage On Employee Type
-case $EMP_CHECK in
-	0)
-		echo "Employee Present"
-		echo "Daily Wage = $((RATE_PER_HOUR * FULL_TIME)) "
-		;;
-	1)
-		echo "Part Time Employee"
-		echo "Daily Wage = $((RATE_PER_HOUR * PART_TIME))"
-		;;
-	2)
-		echo "Employee Absent"
-		echo "Daily Wage = 0"
-		;;
-esac
+#! Declaring Variable
+day=1
+
+#! Calculating Monthly Wage On Employee Type
+while [ $day -le 20 ]
+do
+	emp_Check=$((RANDOM % 3))
+	case $emp_Check in
+		0)
+			salary=$((salary + $((RATE_PER_HOUR * FULL_TIME)) ))
+			;;
+		1)
+			salary=$((salary + $((RATE_PER_HOUR * PART_TIME)) ))
+			;;
+		2)
+			salary=$((salary + 0))
+			;;
+	esac
+	((day++))
+done
+echo "Employee Monthly salary = $salary"

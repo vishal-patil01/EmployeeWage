16186717af2ea2a48c346eb54345c0e10a9ace79
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-21 17:46:18 +0530

    ADDED : Calculating Wages On Hour Basis

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,23 +8,28 @@ readonly PART_TIME=4
 readonly FULL_TIME=8
 
 #! Declaring Variable
-day=1
-
+day=0
+hours=0
 #! Calculating Monthly Wage On Employee Type
-while [ $day -le 20 ]
+while [[ $day -lt 20 && $hours -lt 100 ]]
 do
 	emp_Check=$((RANDOM % 3))
 	case $emp_Check in
 		0)
 			salary=$((salary + $((RATE_PER_HOUR * FULL_TIME)) ))
+			hours=$((hours + FULL_TIME))	
+			((day++))
 			;;
 		1)
 			salary=$((salary + $((RATE_PER_HOUR * PART_TIME)) ))
+			hours=$((hours + PART_TIME))
+			((day++))
 			;;
 		2)
 			salary=$((salary + 0))
+			hours=$((hours + 0))
 			;;
 	esac
-	((day++))
 done
 echo "Employee Monthly salary = $salary"
+

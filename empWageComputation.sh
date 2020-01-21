5601f21d878920f360c665128d6c2c8ec018edc8
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-21 16:33:17 +0530

    ADDED : Part Time Emplyee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,14 +3,20 @@
 echo -e "Welcome To EmployeeWage"
 
 #! Declaring Constants
-WAGE_PER_HOUR=20
-FULL_DAY_HOUR=8 
+readonly RATE_PER_HOUR=20
+readonly EMP_CHECK=$((RANDOM % 3))
+readonly PART_TIME=4
+readonly FULL_TIME=8
 
-#! Checking Employee Is Present or Not
-if  [ $((RANDOM % 2 )) -eq 0 ]
+#! Calculating Wage On Employee Type
+if [ $EMP_CHECK -eq 0 ]
 then
 	echo "Employee Present"
-	echo "Daily Wage = $((WAGE_PER_HOUR * FULL_DAY_HOUR)) "
+	echo "Daily Wage = $((RATE_PER_HOUR * FULL_TIME)) "
+elif [ $EMP_CHECK -eq 1 ]
+then
+	echo "Part Time Employee"
+	echo "Daily Wage = $((RATE_PER_HOUR * PART_TIME))"
 else
 	echo "Employee Absent"
 	echo "Daily Wage = 0"
